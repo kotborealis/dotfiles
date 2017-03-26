7c4419cbb67d27ae7481e12168634a79cdec43df
kotborealis <kotborealis@gmail.com>
2017-03-26 16:06:20 +0300

    :thinking:

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-ln -fs $(pwd)/.bash_aliases $HOME/.bash_aliases
-ln -fs $(pwd)/.bashrc $HOME/.bashrc
-ln -fs $(pwd)/.gitconfig $HOME/.gitconfig
-ln -fs $(pwd)/.zshrc $HOME/.zshrc
-ln -fs $(pwd)/.jetbrains $HOME/.jetbrains
-ln -fs $(pwd)/.oh-my-zsh/custom/kotborealis.zsh-theme $HOME/.oh-my-zsh/custom/kotborealis.zsh-theme
+stow bash
+stow git
+stow zsh
