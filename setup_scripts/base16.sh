ff9191cc4a3925bfa42a3012005aa4428660b8cc
kotborealis <kotborealis@gmail.com>
2019-01-05 14:37:39 +0300

    git gud

--- setup_scripts/base16.sh
+++ setup_scripts/base16.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-sudo pip install base16-shell-preview
+sudo pip3 install base16-shell-preview
+git clone https://github.com/AuditeMarlow/base16-manager ~/.config/base16-manager
+cd ~/.config/base16-manager && sudo make install
+base16-manager install chriskempson/base16-shell
+base16-manager install chriskempson/base16-vim
+base16-manager install 0xdec/base16-rofi
