f90cb0e9cab1aa2a5aa0544868e9cb949c69e9be
kotborealis <kotborealis@gmail.com>
2019-01-05 19:19:35 +0300

    git gud

--- setup_scripts/base16.sh
+++ setup_scripts/base16.sh
@@ -6,6 +6,6 @@ pip3 install base16-shell-preview
 
 # rofi
 git clone https://github.com/0xdec/base16-rofi.git ~/.config/rofi/base16-themes/
-mv ~/.config/rofi/base16-themes/themes ~/.config/rofi/themes/
+mkdir -p ~/.local/share/rofi/
+mv ~/.config/rofi/base16-themes/themes ~/.local/share/rofi/
 rm -rf ~/.config/rofi/base16-themes/
-rofi -theme base16-default-dark
