0f50324d793ab29ac6ccd9801c6b6b03306d1204
kotborealis <kotborealis@gmail.com>
2020-07-25 23:04:34 +0300

    added autostart scripts

--- install.sh
+++ install.sh
@@ -12,5 +12,9 @@ stow octave
 stow zathura
 stow rofi
 
+mkdir -p ~/.config/autostart/
+ln -s ./autostart/.config/autostart/switch-layout.desktop ~/.config/autostart/switch-layout.desktop
+ln -s ./autostart/.config/autostart/Telegram.desktop ~/.config/autostart/Telegram.desktop
+
 ./setup_scripts/plug.vim.sh
 ./setup_scripts/base16.sh
